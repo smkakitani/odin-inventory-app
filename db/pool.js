2c4e5a2ca92afbbb785445a6ba6ca1fac3ad7053
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-10-29 19:41:28 -0300

    fix comment and add file to ./db

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+// Connection with local database
 module.exports = new Pool({
   connectionString: process.env.CONNECTION_STRING_LOCAL_DB
 });
